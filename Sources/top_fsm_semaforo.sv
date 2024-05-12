321b109fee6e126be526c7874275632ca28e0ce8
jorellana75 <162061174+jorellana75@users.noreply.github.com>
2024-05-12 15:08:13 -0600

    top_fsm_semaforo.sv

--- Sources/top_fsm_semaforo.sv
+++ Sources/top_fsm_semaforo.sv
@@ -23,31 +23,32 @@
 module top_fsm_semaforo(
 
     input logic CLK100MHZ,
-    input logic [2:0]sw,
+    input logic [3:0]sw,
     output logic [6:0]LED
 
     );
     
     logic internal_psc_clock;
     
-    // Instanciaci髇 del m骴ulo de generaci髇 de reloj (clck_psc)
+    // Instanciaci贸n del m贸dulo de generaci贸n de reloj (clck_psc)
     clck_psc my_clck (
         (CLK100MHZ),
         (internal_psc_clock)
     );
 
-    // Instanciaci髇 del m骴ulo fsm_semaforo
+    // Instanciaci贸n del m贸dulo fsm_semaforo
     fsm_semaforo FSM(
         .clk(internal_psc_clock), 
         .TA(sw[0]), 
         .TB(sw[1]), 
-        .E(sw[2]), 
+        .E(sw[2]),
+        .R(sw[3]), 
         .verde({LED[3], LED[0]}), 
         .amarillo({LED[4], LED[1]}), 
         .rojo({LED[5], LED[2]})
     );
 
-    // Asignaci髇 para conectar el reloj interno a un LED
+    // Asignaci贸n para conectar el reloj interno a un LED
     assign LED[6] = internal_psc_clock;
     
 endmodule
